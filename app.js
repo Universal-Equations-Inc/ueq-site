f113ebddfc656fd58c1e919928337e7cf63ca11f
malkebu-lan <malkebu-lan@uequations.com>
2018-06-16 23:38:52 -0400

    updated enforceHTTPS option #vPgzj0Zg

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 if(process.env.NODE_ENV === 'production'){
-console.log("enforcing HTTPS redirect")
+console.log("enforcing HTTPS redirect");
 app.use(function(request, response){
   if(!request.secure){
     response.redirect("https://" + request.headers.host + request.url);
