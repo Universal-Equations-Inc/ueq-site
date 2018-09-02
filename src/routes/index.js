8ffae87a3d41fd83c5e34650a64f4b69e1049437
malkebu-lan <malkebu-lan@uequations.com>
2018-09-02 09:18:41 -0400

    add about us page

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,6 +19,42 @@ const SHOP_URL = 'https://shop.uequations.com';
 const GOVT_INQ_URL = "https://www.fedbizdirectory.com/business/d/080587713";
 const COMCAST_URL = "https://my.xfinity.com";
 
+/* GET about us page. */
+router.get('/about', function (req, res, next) {
+    res.render('about', {
+            title: 'Universal Equations, Inc. | Welcome'
+            ,
+            description: 'Universal Equations, Inc. | The Best in Strategy, Consulting, Digital, Technology and Operations Services'
+            ,
+            keywords: 'cloud, cloud computing, oracle cloud, cloud services, cloud hosting, private cloud, iot devices, hybrid cloud, oracle cloud services, cloud technology'
+            ,
+            hours: 'Opening Hours:  Mon - Fri : 9.00 am - 6.30 pm'
+            ,
+            email_label: 'Mail Us Today'
+            ,
+            email_address: 'support@uequations.com'
+            ,
+            location_label: 'Company Location'
+            ,
+            location_address: 'Greater Philadelphia Area, PA'
+            ,
+            home: 'Home'
+            ,
+            blog: 'Blog'
+            ,
+            shop: 'Shop'
+            ,
+            phone_label: ' +(267) 775 3011'
+            ,
+            phone_href: 'tel:+(267) 775 3011'
+            ,
+            office_365: 'Microsoft Office 365'
+            ,
+            pitch: 'Select from a broad selection of enterprise-grade Cloud Computing solutions.'
+        }
+    );
+});
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
